ad979f5cb63162352bec89d8510fa6e7a7d509a8
Anurag <anuragf45@gmail.com>
2022-09-13 17:22:57 +0530

    college

--- src/controllers/collegeController.js
+++ src/controllers/collegeController.js
@@ -3,6 +3,12 @@ const collegeModels = require("../models/collegeModels");
 let createCollege = async function (req, res) {
     try {
         let data = req.body;
+
+        if (!Object.keys(data).length) return res.send("Enter some data to create college");
+        if (!data.name) return res.send("Name is Mandatory");
+        if (!data.fullName) return res.send("FullName is mandatory");
+
+
         let createdData = await collegeModels.create(data);
         res.send(createdData);
     }

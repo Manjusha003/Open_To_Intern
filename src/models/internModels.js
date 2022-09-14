57b84dd185bef6123c9da476815efe62d2f8b811
Anurag <anuragf45@gmail.com>
2022-09-14 15:33:19 +0530

    update of trim

--- src/models/internModels.js
+++ src/models/internModels.js
@@ -4,9 +4,9 @@ const ObjectId = mongoose.Schema.Types.ObjectId; //need to check;
 
 
 const internSchema = new mongoose.Schema({
-    name: { type: String, reguired: true },
-    email: { type: String, unique: true, required: true },
-    mobile: { type: Number, required: true, unique: true },
+    name: { type: String, reguired: true ,trim:true},
+    email: { type: String, unique: true, required: true ,trim:true},
+    mobile: { type: Number, required: true, unique: true,trim:true },
     collegeId: { type: ObjectId, ref:'college' },
     isDeleted: { type: Boolean, default: false }
 
